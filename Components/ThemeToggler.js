a675e933994f101ba7e923d10ec9617cba60697a
prasanthkj <97456383+prasanthkj@users.noreply.github.com>
2022-01-10 21:06:33 +0530

    Update ThemeToggler.js

--- Components/ThemeToggler.js
+++ Components/ThemeToggler.js
@@ -2,17 +2,16 @@ import React, {useContext} from "react";
 import ThemeContext from "../Context/ThemeContext";
 
 const ThemeToggler = () => {
-    const [themeMode, setThemeMode] = useContext(ThemeContext);
+    const themeMode = useContext(ThemeContext);
 
     return (
         <div
-        onClick = {()=>{
-            setThemeMode(themeMode === "light" ? "dark" : "light")
-        }}
+         onClick = {()=>{
+             setThemeMode(themeMode === "light" ? "dark" : "light")
+         }}
         >
             <span>{themeMode === "light" ? "Turn Off" : "Lights On"}</span>
         </div>
     )
 }
-
 export default ThemeToggler;
